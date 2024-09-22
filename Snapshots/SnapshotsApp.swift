77da9dd7cc2f3459e6514c16abf8704660e705ae
Anton Bugera <anton.bugera@ioki.com>
2024-09-22 15:30:17 +0200

    Add all

---
+++ Snapshots/SnapshotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapshotsApp.swift
+//  Snapshots
+//
+//  Created by Anton Bugera on 22.09.24.
+//
+
+import SwiftUI
+
+@main
+struct SnapshotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
